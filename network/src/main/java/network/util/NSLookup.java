70905883f3f6dbfbc92fe03534edd30a98adf8f1
Yuna-commits <ksm13563661@gmail.com>
2025-07-31 17:52:13 +0900

    network push

---
+++ network/src/main/java/network/util/NSLookup.java
@@ -0,0 +1,44 @@
+package network.util;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/*
+ * nslookup 구현 -> 입력한 DNS에 대응하는 IP 주소를 보여줌
+ */
+public class NSLookup {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			// DNS 입력
+			String line = inputDNS(scanner);
+			if ("quit".equals(line)) {
+				break;
+			}
+			try {
+				// 등록된 모든 IP 주소를 배열로 반환
+				InetAddress[] InetAddresses = InetAddress.getAllByName(line);
+				for (InetAddress InetAddress : InetAddresses) {
+					String ipAddress = (InetAddress.toString()).substring(line.length() + 1);
+
+					System.out.println(line + " : " + ipAddress);
+				}
+			} catch (UnknownHostException ex) {
+				System.out.println("알려진 호스트가 없습니다.");
+			}
+		}
+
+		scanner.close();
+	}
+
+	public static String inputDNS(Scanner scanner) {
+		System.out.print("> ");
+		String line = scanner.nextLine();
+
+		return line;
+	}
+
+}
