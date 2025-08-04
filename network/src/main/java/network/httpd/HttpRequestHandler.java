0e590a82ad7a7874267309ef76595b8de5b6dd88
Yuna-commits <ksm13563661@gmail.com>
2025-08-04 17:09:44 +0900

    writeSimpleMessage() 추가

--- network/src/main/java/network/httpd/HttpRequestHandler.java
+++ network/src/main/java/network/httpd/HttpRequestHandler.java
@@ -8,9 +8,9 @@ import java.nio.file.Files;
 public class HttpRequestHandler extends Thread {
 	private static final String DOCUMENT_ROOT = "./webapp";
 	private static final String DEFAULT_FILE = "/index.html";
-	private static final String ERROE_400_FILE = "/error/400.html";
-	private static final String ERROE_404_FILE = "/error/404.html";
-	
+	private static final String ERROR_400_FILE = "/error/400.html";
+	private static final String ERROR_404_FILE = "/error/404.html";
+
 	private static final String OK = "200 OK";
 	private static final String BAD_REQUEST = "400 Bad Request";
 	private static final String FILE_NOT_FOUND = "404 File Not Found";
@@ -101,10 +101,10 @@ public class HttpRequestHandler extends Thread {
 	}
 
 	public void response400(OutputStream outputStream) throws IOException {
-		File file = new File(DOCUMENT_ROOT + ERROE_400_FILE);
+		File file = new File(DOCUMENT_ROOT+ERROR_400_FILE);
 
 		if (!file.exists()) {
-			response404(outputStream);
+			writeSimpleMessage(outputStream, BAD_REQUEST, "잘못된 요청입니다.");
 			return;
 		}
 
@@ -112,9 +112,10 @@ public class HttpRequestHandler extends Thread {
 	}
 
 	public void response404(OutputStream outputStream) throws IOException {
-		File file = new File(DOCUMENT_ROOT + ERROE_404_FILE);
+		File file = new File(DOCUMENT_ROOT + ERROR_404_FILE);
 
 		if (!file.exists()) {
+			writeSimpleMessage(outputStream, FILE_NOT_FOUND, "요청한 리소스가 존재하지 않습니다.");
 			return;
 		}
 
@@ -125,14 +126,34 @@ public class HttpRequestHandler extends Thread {
 		// NIO(New I/O)
 		byte[] body = Files.readAllBytes(file.toPath());// IOException
 		String contentType = Files.probeContentType(file.toPath());
-		
+
 		// Response Header
-		outputStream.write(("HTTP/1.1" + message + "\n").getBytes("utf-8"));
+		outputStream.write(("HTTP/1.1 " + message + "\n").getBytes("utf-8"));
 		// Content-Type -> text/html이면 css 적용 x
 		outputStream.write(("Content-Type:" + contentType + "; charset=utf-8\n").getBytes("utf-8"));
 		outputStream.write("\n".getBytes("utf-8"));// 비어있는 개행
+
 		// Response Body
 		outputStream.write(body);
+	}
+
+	// 400.html, 404.html 파일이 없는 경우
+	public void writeSimpleMessage(OutputStream outputStream, String status, String message) throws IOException {
+		String body = "<html>" + 
+				"<head><title>" + status + "</title></head>" + 
+				"<body><h1>" + status + "</h1><p>"+message+"</body>" + 
+				"</html>";
+
+		byte[] bodyBytes = body.getBytes("utf-8");
+
+		// Response Header
+		outputStream.write(("HTTP/1.1 " + status + "\n").getBytes("utf-8"));
+		outputStream.write(("Content-Type: text/html; charset=utf-8\n").getBytes("utf-8"));
+		outputStream.write(("Content-Length: " + bodyBytes + "\n").getBytes("utf-8"));
+		outputStream.write("\n".getBytes("utf-8"));// 비어있는 개행
+
+		// Response Body
+		outputStream.write(bodyBytes);
 	}
 
 	public void consoleLog(String message) {
