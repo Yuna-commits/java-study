859dff420dbd6d201092c2bd2312d8fa31e517fd
Yuna-commits <ksm13563661@gmail.com>
2025-09-04 15:48:22 +0900

    strategy pattern 연습

---
+++ designpattern-practices/src/main/java/behavioral/strategy/Client.java
@@ -0,0 +1,83 @@
+package behavioral.strategy;
+
+//import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) {
+//		add();
+//		subtract();
+//		multiply();
+//		divide();
+
+		CalculateContext cc = new CalculateContext();
+
+		// Implements CalculateStrategy - AddStrategy
+		cc.operation(new AddStrategy());
+
+		// Anonymous Class - SubtractStrategy
+		cc.operation(new CalculateStrategy() {
+			@Override
+			public int calculate(int val1, int val2) {
+				return val1 - val2;
+			}
+		});
+
+		// Lambda Function, Arrow Function - MultiplyStrategy
+		CalculateStrategy cs = (val1, val2) -> {
+			return val1 * val2;
+		};
+		cc.operation(cs);
+
+		// Implements CalculateStrategy - DivideStrategy
+		cc.operation((val1, val2) -> val1 / val2);
+	}
+
+//	public static void add() {
+//		Scanner scanner = new Scanner(System.in);
+//
+//		System.out.print("(val1, val2) > ");
+//
+//		int val1 = scanner.nextInt();
+//		int val2 = scanner.nextInt();
+//		int result = val1 + val2;
+//
+//		System.out.println(result);
+//	}
+//
+//	public static void subtract() {
+//		Scanner scanner = new Scanner(System.in);
+//
+//		System.out.print("(val1, val2) > ");
+//
+//		int val1 = scanner.nextInt();
+//		int val2 = scanner.nextInt();
+//		int result = val1 - val2;
+//
+//		System.out.println(result);
+//	}
+//
+//	public static void multiply() {
+//		Scanner scanner = new Scanner(System.in);
+//
+//		System.out.print("(val1, val2) > ");
+//
+//		int val1 = scanner.nextInt();
+//		int val2 = scanner.nextInt();
+//		int result = val1 * val2;
+//
+//		System.out.println(result);
+//	}
+//
+//	public static void divide() {
+//		Scanner scanner = new Scanner(System.in);
+//
+//		System.out.print("(val1, val2) > ");
+//
+//		int val1 = scanner.nextInt();
+//		int val2 = scanner.nextInt();
+//		int result = val1 / val2;
+//
+//		System.out.println(result);
+//	}
+}
